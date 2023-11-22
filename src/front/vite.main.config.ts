f2f66b801a6b79e78c515b5d2d53f9715108d41d
whitehorse <gabrio.lina@hotmail.com>
2023-11-22 17:35:29 -0300

    fix(vite): remove sqlite3 from vite

--- src/front/vite.main.config.ts
+++ src/front/vite.main.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ["better-sqlite3"],
+    },
+  },
   resolve: {
     // Some libs that can run in both Web and Node.js, such as `axios`, we need to tell Vite to build them in Node.js.
     browserField: false,
