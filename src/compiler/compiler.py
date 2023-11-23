fe6e95201b82a26b3075af154534742347f81c64
Cristiane Andrade <99192794+andrdcris@users.noreply.github.com>
2023-11-22 22:58:11 -0300

    adicionando arvore sintatica ao analizador lexico

--- src/compiler/compiler.py
+++ src/compiler/compiler.py
@@ -1,17 +1,27 @@
 from lexic.lexic_analyzer import Lexic
 from syntatic.syntatic_analyzer import Syntatic
-
-
+from syntatic.tree_generator import NoInterno, NoFolha
 
 class Compiler:
 
     def __init__(self, code):
         self.code = code
-        self.tokes = []
+        self.tokens = []
 
-    
     def compile(self):
         lexic_analyzer = Lexic(self.code)
         lexic_analyzer.lexic()
         syntatic_analyzer = Syntatic(lexic_analyzer.token_list)
-        syntatic_analyzer.analyze()
+        tree = syntatic_analyzer.analyze()
+        self.print_tree(tree)
+
+    def print_tree(self, node, level=0):
+        print('  ' * level + str(node))
+        if isinstance(node, NoInterno):
+            for key, value in node.d.items():
+                if isinstance(value, list):
+                    for item in value:
+                        self.print_tree(item, level + 1)
+                else:
+                    self.print_tree(value, level + 1)
+
