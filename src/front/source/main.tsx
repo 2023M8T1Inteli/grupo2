3cdaa4a1f2ce380f9560ccaff2ff5e524d205bf9
Sergio Lucas <99209004+SergioLucasINT@users.noreply.github.com>
2023-11-30 11:58:30 -0300

    VLibras integrado

--- src/front/source/main.tsx
+++ src/front/source/main.tsx
@@ -1,10 +1,15 @@
 import ReactDOM from "react-dom/client";
 import Router from "./routes/router";
 import { BrowserRouter } from "react-router-dom";
+import VLibras from "@djpfs/react-vlibras";
+import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <Router />
-  </BrowserRouter>,
+  <React.Fragment>
+    <VLibras forceOnload={true} />
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  </React.Fragment>
 );
 
